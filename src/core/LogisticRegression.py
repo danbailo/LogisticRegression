b25d33d92194e3810ed64b11f3c53e058e3f35f8
Daniel Bailo <danbailoufms@gmail.com>
2019-11-12 19:14:04 -0400

    separando dados para treino e validacao

---
+++ src/core/LogisticRegression.py
@@ -0,0 +1,72 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw 
+import glob
+from tqdm import tqdm
+
+plt.rcParams['figure.figsize'] = (10, 7)
+plt.rcParams['axes.grid'] = True
+
+np.random.seed(1)
+
+class LogisticRegression:
+	def __init__(self, X, Y, lr, m, epochs, Thetas):
+		self.X = X
+		self.Y = Y
+		self.lr = lr
+		self.m = m
+		self.epochs = epochs
+		self.Thetas = Thetas
+
+	def split_data(self, percentage):		
+		smaller = min(len(self.Y[self.Y==0]), len(self.Y[self.Y==1]))
+
+		indexes = []
+		self.X_train, self.Y_train = [], []
+		self.X_validate, self.Y_validate = [], []
+
+		state = 1
+		while True:
+			index = np.random.choice(np.arange(0,len(self.X)))
+			while True:
+				if index in indexes:
+					index = np.random.choice(np.arange(0,len(self.X)))
+				else: break			
+			indexes.append(index)
+			if state == 1:
+				X_train.append(self.X[index])
+				Y_train.append(self.Y[index][0])
+				if len(X_train) == smaller:				
+					state = 2
+			elif state == 2:
+				X_validate.append(self.X[index])
+				Y_validate.append(self.Y[index][0])
+				if len(X_validate) == smaller:
+					state = 3				
+			else: break
+
+	def fit(self):
+		loss = []
+		acc = []
+		for _ in tqdm(range(self.epochs)):
+			Z = self.X.dot(self.Thetas)
+			H_theta = np.divide(1, 1+np.exp(-Z))
+			E = H_theta - self.Y
+			self.Thetas = self.Thetas - (self.lr * (self.X.T.dot(E))) 
+			loss.append((1 / self.m) * np.sum(-self.Y * np.log(H_theta) - (1-self.Y) * (np.log(1 - H_theta))))
+			acc.append(np.sum((H_theta >= 0.5) == self.Y) / self.m)
+		return loss, acc
+
+	def predict(self):
+		loss = []
+		acc = []
+		for _ in tqdm(range(self.epochs)):
+			Z = self.X.dot(self.Thetas)
+			H_theta = np.divide(1, 1+np.exp(-Z))
+			E = H_theta - self.Y
+			self.Thetas = self.Thetas - (self.lr * (self.X.T.dot(E))) 
+			loss.append((1 / self.m) * np.sum(-self.Y * np.log(H_theta) - (1-self.Y) * (np.log(1 - H_theta))))
+			acc.append(np.sum((H_theta >= 0.5) == self.Y) / self.m)
+		return loss, acc
---
+++ src/main.py
@@ -0,0 +1,74 @@
+from core import LogisticRegression
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw 
+import glob
+
+plt.rcParams['figure.figsize'] = (10, 7)
+plt.rcParams['axes.grid'] = True
+
+if __name__=="__main__":
+	dir_cat = '../input/train/cat/*.png'
+	dir_noncat = '../input/train/noncat/*.png'
+	
+	X = []
+	Y = []
+	for cat in glob.glob(dir_cat):
+		img = np.asarray(Image.open(cat))
+		img = np.reshape(img, -1)
+		X.append(img)
+		Y.append(1)
+
+	#-1 multiplica pelo grid(x1,x2,x3) da imagem, "desempacota" e multiplica - 64x64x3 = 12288
+	for noncat in glob.glob(dir_noncat):
+		img = np.asarray(Image.open(noncat))
+		img = np.reshape(img, -1)
+		X.append(img)
+		Y.append(0) 	
+
+	X = np.asarray(X)
+	Y = np.asarray(Y)
+	m = X.shape[0]
+	lr = 0.0001
+
+	X = X/255
+	X = np.insert(X, obj=0, values=1, axis=1)
+	Y = np.expand_dims(Y, axis=1)
+	Thetas = np.zeros((X.shape[1],1))
+	
+	logistic_regression = LogisticRegression(
+		X = X,
+		Y = Y,
+		lr = lr,
+		m = m,
+		epochs = 1500,
+		Thetas = Thetas
+	)
+
+	X_train, Y_train, X_validate, Y_validate = logistic_regression.split_data(0.5)
+
+	print(len(X_train))
+	print(len(Y_train))
+	print(len(X_validate))
+	print(len(Y_validate))
+	# exit()
+
+	loss_train, acc_train = logistic_regression.fit()
+	# loss_validate, acc_validate = logistic_regression.predict()
+
+	plt.figure()
+	plt.plot(loss_train, label=fr"$leaning rate = {lr}$")
+	# plt.plot(loss_validate, label=fr"$leaning rate = {lr}$")
+	plt.legend(loc='center left', bbox_to_anchor=(1.0, 0.5))
+	plt.xlabel('epochs')
+	plt.ylabel(r'J($\theta$)')	
+
+	plt.figure()
+	plt.plot(acc_train)
+	# plt.plot(acc_validate)
+	plt.xlabel('epochs')
+	plt.ylabel(r'acc')		
+	
+	plt.show()
